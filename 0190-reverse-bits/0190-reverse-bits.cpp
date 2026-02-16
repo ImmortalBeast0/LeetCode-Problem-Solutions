63b175bd89f16d97aaa287ce058892f1d0f964d5
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-02-16 09:38:57 +0530

    Time: 0 ms (100%), Space: 8 MB (99.59%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        string s = bitset<32> (n).to_string();
-        reverse(s.begin(),s.end());
-        int sol = stoi(s,nullptr,2);
-        return sol;  
+        return __builtin_bitreverse32(n);
     }
 };
