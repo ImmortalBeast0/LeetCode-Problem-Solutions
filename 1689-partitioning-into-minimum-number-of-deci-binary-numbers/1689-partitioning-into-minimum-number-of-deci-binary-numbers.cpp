ab2cb7b1b588acadc5eff5e2e456fa5e7cbe7d52
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-03-04 12:23:42 +0530

    Time: 36 ms (6.97%), Space: 18.2 MB (16.09%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -32,7 +32,7 @@ public:
             bool flag = true;
             for(int i=0;i<ans.length();i++)
                 if(ans[i] >= '1'){
-                    ans[i] = ((ans[i] - '0' )- 1 )+ '0';
+                    ans[i] -= 1;
                     flag = false;
                 }
             
