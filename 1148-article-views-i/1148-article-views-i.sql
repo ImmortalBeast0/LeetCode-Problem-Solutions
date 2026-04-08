6103058f2ba4febbedd8b185de86bed8991ad497
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-04-08 21:09:57 +0530

    Time: 380 ms (86.24%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,8 +1,4 @@
-SELECT DISTINCT author_id as id
-FROM Views v
-WHERE
-(SELECT COUNT(*)
+SELECT DISTINCT author_id as id 
 FROM Views
-WHERE v.author_id = author_id 
-AND v.author_id = viewer_id) >= 1
+WHERE author_id = viewer_id
 ORDER BY author_id;
