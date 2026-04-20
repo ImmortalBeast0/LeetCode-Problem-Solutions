a079afab9291a0e23953ba4678ef6f1948612c24
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-04-20 11:39:19 +0530

    Time: 23 ms (12.1%), Space: 102.3 MB (10.83%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int n,m;
-
     int get(int key ,vector<int>& nums){
         int ans = m+1;
-        int lb = 0,ub = n-1;
+        int lb = 0,ub = n-1,mi;
         while(lb <= ub){
-            int mi = (ub - lb)/2 + lb;
+            mi = (ub - lb)/2 + lb;
             if(nums[mi] <= key){
                 ub = mi - 1;
                 ans = mi;
-            }else{
+            }else
                 lb = mi + 1;
-            }
         }
         return ans;
     }
@@ -21,11 +19,9 @@ public:
         n = nums1.size();
         m = nums2.size();
         int sol = 0;
-        for(int i=m-1;i>=0;i--){
-            int rt = nums2[i];
-            int idx = get(rt,nums1);
-            if(idx <= i)
-                sol = max(sol,i - idx);
+        for(int j=m-1;j>=0;j--){
+            int i = get(nums2[j],nums1);
+            sol = max(sol,max(j - i,0));
         }
 
         return sol;
