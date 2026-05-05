5cacf664872f26f1f69e12b8c713685d143566fe
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-05-05 06:31:25 +0530

    Time: 0 ms (100%), Space: 21.8 MB (80.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& b) {
-        //row check
+        //All in One 
+        int rows[9] ={},cols[9] ={},boxes[9] ={};
+
         for(int i=0;i<9;i++){
-            int freq[10] = {0}; 
-            for(int j=0;j<9;j++)
-                if(b[i][j] != '.')
-                freq[b[i][j] - '0']+=1;
-            for(int j=0;j<10;j++)
-                if(freq[j] > 1)
+            for(int j=0;j<9;j++){
+
+                if(b[i][j] == '.')
+                    continue;
+
+                int id = b[i][j] - '1';
+                int bit = 1 << id;
+                int box = (i / 3) * 3 + (j / 3);
+                if(rows[i] & bit | cols[j] & bit | boxes[box] & bit)
                     return false;
-        }
 
-        //column check
-        for(int j=0;j<9;j++){
-            int freq[10] = {0};
-            for(int i=0;i<9;i++)
-                if(b[i][j] != '.')
-                freq[b[i][j] - '0']++;
-            for(int j=0;j<10;j++)
-                if(freq[j] > 1)
-                    return false;
-        }
-
-        //3 X 3 grid check
-        for(int i=0;i<9;i+=3){
-            for(int j=0;j<9;j+=3){
-
-                int freq[10] = {0};
-                for(int r=i;r<i+3;r++)
-                    for(int c=j;c<j+3;c++)
-                        if(b[r][c] != '.')
-                        freq[b[r][c] - '0']++;
-
-                for(int k=0;k<10;k++)
-                    if(freq[k] > 1)
-                        return false;
+                rows[i] |= bit;
+                cols[j] |= bit;
+                boxes[box] |= bit; 
             }
         }
 
