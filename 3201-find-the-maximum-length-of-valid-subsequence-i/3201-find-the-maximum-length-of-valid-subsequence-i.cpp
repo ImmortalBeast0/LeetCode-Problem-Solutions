b858381d14bdd35bb12fb9e93f5e01f8bff9f397
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-05-19 12:03:51 +0530

    Time: 0 ms (100%), Space: 96.1 MB (61.37%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    int n;
-    vector<int> nums;
-    int dp[(int)(2*1e5+5)][4];
-    int rec(int i ,int last){
-        if(i == n)
-            return 0;
+    int maximumLength(vector<int>& nums) {
 
-        if(dp[i][last+1] != -1)
-            return dp[i][last+1];
+        int oddcnt = 0 ,evencnt = 0;
+        int alteven = 0 , altodd = 0;
 
-        int ans = -1e9;
-        if(last == -1 || nums[i] != last)
-            ans = max(ans,1 + rec(i+1,nums[i]));
-        
-        ans = max(ans ,rec(i+1,last));
-
-        return dp[i][last+1] = ans;
-    }
-
-    int maximumLength(vector<int>& arr) {
-        n = arr.size();
-        int ones = 0;
-        for(int x : arr){
-            ones += x % 2;
-            nums.push_back(x % 2);
+        for(int x : nums){
+            int num = x % 2;
+            if(num){
+                oddcnt += 1;
+                altodd = alteven + 1;
+            }else{
+                evencnt += 1;
+                alteven = altodd + 1;
+            }
         }
-        
-        int ans = max(ones,n - ones);
-        memset(dp,-1,sizeof(dp));
-        return max(ans,rec(0,-1));
+
+        return max({oddcnt,evencnt,alteven,altodd});
     }
 };
