fa0c2c51d109d2183bde80dff71680e8f060f73d
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-05-19 09:47:56 +0530

    Time: 47 ms (13.05%), Space: 69.7 MB (10.22%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mp;
+        for(int x : nums1)
+            mp[x]++;
+        sort(nums2.begin(),nums2.end());
+        for(int x : nums2){
+            if(mp.find(x) != mp.end())
+                return x;
+        }
+
+        return -1;
+    }
+};
