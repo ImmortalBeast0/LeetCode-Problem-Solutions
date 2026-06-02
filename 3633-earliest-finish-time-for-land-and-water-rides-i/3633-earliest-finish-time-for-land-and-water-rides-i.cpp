a9c9855aab43cc8e8918b479f59063c4bda6cb66
Moni <168914947+ImmortalBeast0@users.noreply.github.com>
2026-06-02 21:12:05 +0530

    Time: 12 ms (29.96%), Space: 100.2 MB (6.33%) - LeetHub

---
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod ((int)1e9+7)
+
+#define vi vector<int>
+#define vii vector<vi>
+#define pi pair<int,int>
+#define vpi vector<pi>
+
+#define F first
+#define S second
+#define pb push_back
+#define bp pop_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+
+class Solution {
+public:
+
+    int bs(vii& arr ,int end){
+        int n = arr.size();
+        int lb = 0 ,ub = n-1;
+
+        int ans = ub;
+        while(lb <= ub){
+            int mi = (lb + ub)/2;
+            if(arr[mi][0] == end)
+                return mi;
+            else if(arr[mi][0] < end)
+                lb = mi + 1;
+            else
+                ub = mi - 1;
+        }
+
+        return lb - 1;
+    }
+
+    int earliestFinishTime(vector<int>& lst, vector<int>& ld, vector<int>& wst, vector<int>& wd) {
+        
+        int n = lst.size();
+        int m = wst.size();
+
+        vii land ,water;
+        for(int i=0;i<n;i++)
+            land.pb({lst[i],ld[i]});
+
+        for(int i=0;i<m;i++)
+            water.pb({wst[i],wd[i]});
+
+        sort(all(water));
+        sort(all(land));
+
+        vi pxl(n);
+        pxl[0] = land[0][1];
+        for(int i=1;i<n;i++)
+            pxl[i] = min(pxl[i-1],land[i][1]);
+
+        vi pxw(m);
+        pxw[0] = water[0][1];
+        for(int i=1;i<m;i++)
+            pxw[i] = min(pxw[i-1],water[i][1]);
+
+        vi sxl(n);
+        sxl[n-1] = land[n-1][0] + land[n-1][1];
+        for(int i=n-2;i>=0;i--)
+            sxl[i] = min(sxl[i+1],land[i][0]+land[i][1]);
+
+        vi sxw(m);
+        sxw[m-1] = water[m-1][0] + water[m-1][1];
+        for(int i=m-2;i>=0;i--)
+            sxw[i] = min(sxw[i+1],water[i][0] + water[i][1]);
+        
+        int ans = 1e9;
+
+        for(int i=0;i<n;i++){
+            int end = land[i][0] + land[i][1];
+            int idx = bs(water,end);
+            if(idx >= 0)
+                ans = min(ans,end+pxw[idx]);
+            if(idx+1 < m)
+                ans = min(ans,sxw[idx+1]);
+        }
+
+        for(int i=0;i<m;i++){
+            int end = water[i][0] + water[i][1];
+            int idx = bs(land,end);
+            if(idx >= 0)
+                ans = min(ans,end+pxl[idx]);
+            if(idx + 1 < n)
+                ans = min(ans,sxl[idx+1]);
+        }
+
+        return ans;
+    }
+};
