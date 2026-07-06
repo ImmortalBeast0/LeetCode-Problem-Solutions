e3ed0af6b60631c8fcb37ef1b4eb2188f2a0bd1c
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-07-06 07:14:16 +0530

    Time: 7 ms (17.35%), Space: 16 MB (12.58%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+
+    bool static cmp(vector<int>& a ,vector<int>& b){
+        if(a[0] == b[0])
+            return a[1] > b[1];
+        return a[0] < b[0];
+    }
+
     int removeCoveredIntervals(vector<vector<int>>& in) {
-        sort(in.begin(),in.end());
-        int n = in.size();
-        bool flag[n];
-        memset(flag,false,sizeof(flag));
-        for(int i=0;i<n;i++){
-            if(flag[i])
-                continue;
-            int a = in[i][0] ,b = in[i][1];
-            for(int j=0;j<n;j++){
-                int c = in[j][0] ,d = in[j][1];
-                if(i != j && c <= a && b <= d){
-                    flag[i] = true;
-                    break;
-                }
+        sort(in.begin(),in.end(),cmp);
+
+        int maxEnd = -1;
+        int sol = 0;
+        //The intervals were sorted
+        //ith ,li is greter than or equal to (i-1)th ,l(i-1)  -  l(i-1) <= li ,then 
+        //if ri is - ri < (r(i-1)) ,means ith covered interval index
+        for(vector<int> i : in){
+            if(maxEnd < i[1]){
+                maxEnd = i[1];
+                sol += 1;
             }
         }
 
-        int cnt = 0;
-        for(bool f : flag)
-            cnt += f;
-        return n - cnt;
+        return sol;
     }
 };
