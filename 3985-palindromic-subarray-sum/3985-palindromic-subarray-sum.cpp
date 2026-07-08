09fa45151c4e9d2e1fc7bf6c799d03a1ce499525
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-07-08 06:56:49 +0530

    Time: 70 ms (20.28%), Space: 244.7 MB (11.24%) - LeetHub

--- 3985-palindromic-subarray-sum/3985-palindromic-subarray-sum.cpp
+++ 3985-palindromic-subarray-sum/3985-palindromic-subarray-sum.cpp
@@ -1,132 +1,64 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#define ll long long
-#define mod ((int)1e9+7)
-
-
-#define vi vector<int>
-#define vii vector<vi>
-#define pi pair<ll,ll>
-#define vpi vector<pi>
-
-#define pb push_back
-#define bp pop_back
-#define F first
-#define S second 
-#define all(x) x.begin(),x.end()
-
 class Solution {
 public:
+    using ll = long long;
+    struct Manacher{
+        vector<int> p;
+        vector<ll> ps;
 
-    //Rolling Hash DS structure
-    struct RHasher{
-        ll B ,MOD;
-        int n;
-        vector<ll> hash;
-        vector<ll> pow;
+        void init(vector<int> nums){
+            vector<int> arr;
+            for(int x : nums){
+                arr.push_back(0);
+                arr.push_back(x);
+            }
 
-        void init(vector<int>& nums ,ll B_,ll MOD_){
-            n = nums.size();
-            B = B_;
-            MOD = MOD_;
-            hash.resize(n);
-            pow.resize(n);
-            build(nums);
+            arr.push_back(0);
+             
+            build(arr);  
         }
 
-        void build(vector<int>& nums){
-            hash[0] = nums[0];
-            pow[0] = 1;
+        void build(vector<int> nums){
+
+            int n = nums.size();
+            p.assign(n,1);
+            ps.assign(n,0);
+
+            ps[0] = nums[0];
+            //boundary box
+            int l = 1, r = 1;
             for(int i=1;i<n;i++){
-                hash[i] = ((hash[i-1] * B) % MOD + nums[i]) % MOD;
-                pow[i] = (pow[i-1] * B) % MOD;
+                ps[i] = (ps[i-1] + nums[i]);
+
+                if(i < r)
+                    p[i] = max(1,min(r-i,p[l + (r - i)]));
+                while(i-p[i] >= 0 && i+p[i] < n && nums[i - p[i]] == nums[i + p[i]])
+                    p[i] += 1;
+
+                if(i + p[i] > r){
+                    l = i - p[i];
+                    r = i + p[i];
+                }
             }
         }
 
-        ll getHash(int l ,int r){
+        ll getValue(int idx){
+            int len = p[idx] - 1;
+            return getSum((idx-len),idx+len);
+        }
+
+        ll getSum(int l ,int r){
             if(l == 0)
-                return hash[r];
-            return ((hash[r] - (hash[l-1] * pow[r - l + 1]) % MOD) + MOD) % MOD;
+                return ps[r];
+            return ps[r] - ps[l-1];
         }
+    }m;
 
-    };
-
-    int n;
-    RHasher normal ,rev;
-    ll BASE = 911382323L;
-    const ll MOD = 1e9 + 7; 
-
-    bool isPalidrome(int l ,int r){
-        ll hash1 = normal.getHash(l,r);
-        ll hash2 = rev.getHash(n-r-1,n-l-1);
-        return hash1 == hash2;
-    }
-
-    ll getSum(vector<int>& nums) {
-
-        n = nums.size();
-
-        //prefix sum
-        vector<ll> ps(n);
-        ps[0] = nums[0];
-        for(int i=1;i<n;i++)
-            ps[i] = nums[i] + ps[i-1];
-
-        //Rolling Hash Object initialzation
-        normal.init(nums,BASE,MOD);
-
-        vector<int> revNums(all(nums));
-        reverse(all(revNums));
-        rev.init(revNums,BASE,MOD);
-
-
-        ll sol = 0;
-        for(int i=0;i<n;i++){
-
-            //Odd Length Palidromic Subarray at center have ith index
-            int len = 0;
-            int maxLen = min(i,(n-1) - (i+1) + 1); //as ith index as center
-            int lb = 0 ,ub = maxLen;
-            while(lb <= ub){
-                int mid = (ub - lb)/2 + lb;
-                if(isPalidrome(i-mid,i+mid)){
-                    len = mid;
-                    lb = mid + 1;
-                }else
-                    ub = mid - 1;
-            }
-
-            sol = max(sol,getSum(i-len,i+len,ps)); //odd at center at i maximized 
-
-            //Even length Palindromic Subarray at ith index in left part of the subarray 
-            if(i+1 >= n || nums[i] != nums[i+1])
-                continue; // if not possible
-            //then
-
-            len = 0;
-            maxLen = min(i,(n-1) - ((i+1)+1) + 1); //just index length formula (r - l + 1)
-            lb = 0 ,ub = maxLen;
-            while(lb <= ub){
-                int mid = (ub - lb)/2 + lb;
-                if(isPalidrome(i-mid,i+1+mid)){
-                    len = mid;
-                    lb = mid + 1;
-                }else
-                    ub = mid - 1;
-            }
-
-            sol = max(sol,getSum(i-len,i+1+len,ps));
+    long long getSum(vector<int>& nums) {
+        m.init(nums);
+        long long sol = 0;
+        for(int i=0;i<2*nums.size()+1;i+=1){
+            sol = max(sol,m.getValue(i));
         }
-
         return sol;
     }
-
-    //Prefix Sum ,Range Sum GET function
-    ll getSum(int l ,int r ,vector<ll>& ps){
-        if(l == 0)
-            return ps[r];
-        return ps[r] - ps[l-1];
-    }
-
 };
