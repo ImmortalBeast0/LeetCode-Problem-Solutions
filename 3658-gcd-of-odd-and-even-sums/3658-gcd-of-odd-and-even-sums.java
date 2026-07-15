2dd98b66816bb251b88ee0d0b3dad6305d9349bb
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-07-15 10:40:53 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.12%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,16 +1,5 @@
 class Solution {
-
-    int gcd(int a ,int b){
-        if(b == 0)
-            return a;
-        return gcd(b,a % b);
-    }
-
     public int gcdOfOddEvenSums(int n) {
-        n *= 2;
-        int sumOfNatural = (n * (n+1))/2;
-        int sumOfEven = (n/2 * (n/2 + 1));
-        int sumOfOdd = sumOfNatural - sumOfEven;
-        return gcd(sumOfOdd,sumOfEven);
+        return n;
     }
 }
