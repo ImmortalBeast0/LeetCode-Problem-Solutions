c714cbb434281055158ee4ecfecc5ba76590e505
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-07-20 10:25:26 +0530

    Time: 25 ms (5.75%), Space: 47.2 MB (44.14%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    void reverseList(List<Integer> arr ,int st ,int en){
+
+        while(st < en){
+            int tmp = arr.get(st);
+            arr.set(st,arr.get(en));
+            arr.set(en,tmp);
+            st += 1;
+            en -= 1;
+        }
+
+    }
+
+    public List<List<Integer>> shiftGrid(int[][] grid, int k){
+        int m = grid.length;
+        int n = grid[0].length;
+        List<Integer> arr = new ArrayList<>();
+        for(int[] a : grid)
+            for(int x : a)
+                arr.add(x);
+
+        k = k % (n * m);
+        
+        reverseList(arr,0,arr.size()-1);
+        reverseList(arr,0,k-1);
+        reverseList(arr,k,arr.size()-1);
+        
+        System.out.println(arr);
+        List<List<Integer>> sol = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            List<Integer> tmp = new ArrayList<>();
+            for(int j=0;j<n;j++)
+                tmp.add(arr.get(i*n+j));
+            sol.add(tmp);
+        }
+
+        return sol;    
+    }   
+}
