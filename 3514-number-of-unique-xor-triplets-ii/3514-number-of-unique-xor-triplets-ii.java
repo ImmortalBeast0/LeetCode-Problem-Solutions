e2fdf9b8e103e55a0f357c3f9b0c6476376e1397
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-07-24 13:42:58 +0530

    Time: 263 ms (49.09%), Space: 47 MB (49.09%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,28 +1,35 @@
 class Solution {
-
-    boolean[][][] dp = new boolean[4][1501][2050];
-    boolean[] seen = new boolean[2050];
-    int cnt = 0;
-
-    void rec(int i, int len, int zor, int[] nums) {
-        if (len == 3) {
-            if (!seen[zor]) {
-                seen[zor] = true;
-                cnt += 1;
-            }
-            return;
-        }
-        if (i >= nums.length)
-            return;
-        if (dp[len][i][zor])
-            return;
-        rec(i + 1, len, zor, nums);
-        rec(i, len + 1, zor ^ nums[i], nums);
-        dp[len][i][zor] = true;
-    }
-
     public int uniqueXorTriplets(int[] nums) {
-        rec(0, 0, 0, nums);
-        return cnt;
+        int mx = nums[0];
+        for(int x : nums)
+            mx = Math.max(mx,x);
+
+        int cnt = 0;
+        while(mx > 0){
+            cnt += 1;
+            mx /= 2;
+        }
+
+        int mx_xor_range = (1 << cnt);
+        boolean[] seen = new boolean[mx_xor_range + 1];
+
+        for(int i=0;i<nums.length;i+=1){
+            for(int j=i;j<nums.length;j+=1)
+                seen[nums[i] ^ nums[j]] = true;
+        }
+
+        boolean[] sol = new boolean[mx_xor_range + 1];
+        for(int i=0;i<=mx_xor_range;i++){
+            if(seen[i]){
+                for(int x : nums)
+                    sol[x ^ i] = true;
+            }
+        }
+
+        int ans = 0;
+        for(boolean b : sol)
+            ans += (b) ? 1 : 0;
+    
+        return ans;
     }
 }
