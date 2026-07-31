23f122de8c7b536eff2094ab5e424ad5b5ae3e5d
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-07-31 20:41:02 +0530

    Time: 63 ms (13.01%), Space: 48.2 MB (17.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -4,20 +4,15 @@ class Solution {
         for(char c : word.toCharArray())
             mp.put(c,mp.getOrDefault(c,0)+1);
 
-        PriorityQueue<List> pq = new PriorityQueue<>((l1,l2) -> Integer.compare((Integer)l2.get(0),(Integer)l1.get(0)));
-
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(Map.Entry<Character,Integer> entry : mp.entrySet())
-            pq.add(Arrays.asList(entry.getValue(),entry.getKey()));
+            pq.add(-entry.getValue());
 
-        int sol = 0;
         int cnt = 0;
-        while(!pq.isEmpty()){
-            List l = pq.poll();
-            sol += (Integer)l.get(0) * ((cnt / 8) + 1);
-            cnt += 1;
-        }
+        int sol = 0;
+        while(!pq.isEmpty())
+            sol += -pq.poll() * (cnt++ / 8 + 1);
 
-        return sol;
+        return sol; 
     }
 }
