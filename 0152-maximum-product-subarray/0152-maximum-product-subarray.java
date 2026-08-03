0090f4a9248a6df2ac665e9691aba09d8cf08bd6
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-08-03 15:48:14 +0530

    Time: 2 ms (71.44%), Space: 47.6 MB (16.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,11 +3,18 @@ class Solution {
         int sol = nums[0];
         int max = 1 ,min = 1;
         for(int i=0;i<nums.length;i++){
-            int t = nums[i] * max;
-            max = Math.max(t,min * nums[i]);
-            max = Math.max(max,nums[i]);
-            min = Math.min(t,min * nums[i]);
-            min = Math.min(min,nums[i]);
+            sol = Math.max(sol,nums[i]);
+            if(nums[i] == 0){
+                max = min = 1;
+                continue;
+            }
+            if(nums[i] < 0){
+                max += min;
+                min = max - min;
+                max = max - min;
+            }
+            max = Math.max(nums[i],max * nums[i]);
+            min = Math.min(nums[i],min * nums[i]);
             sol = Math.max(sol,max);
         }
 
