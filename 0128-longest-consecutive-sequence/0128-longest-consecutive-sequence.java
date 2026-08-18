2ef0545a29f1e08dff13ded724fc4e939eea54ba
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-08-18 23:07:47 +0530

    Time: 30 ms (74.03%), Space: 95.1 MB (72.23%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,57 +1,23 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> exist = new HashSet<>();
+        HashSet<Integer> set = new HashSet<Integer>();
         for(int x : nums)
-            exist.add(x);
+            set.add(x);
 
-        HashMap<Integer,Boolean> vis = new HashMap<>();
-        int n  = nums.length;
-        int cnt = 0; // xth Component explore 
-        int j = 0; //this is a pointer for arr 
-        int[] arr = new int[n];
-        Arrays.fill(arr,-1);
-        for(int i=0;i<n;i++){
-
-            if(!vis.containsKey(nums[i])){
-                // bfs 
-                
-                Deque<Integer> q = new ArrayDeque<>(); 
-                q.addFirst(nums[i]);
-                vis.put(nums[i],true);
-                arr[j++] = cnt;
-
-                while(q.size() > 0){
-                    int x = q.pollFirst();
-                    if(exist.contains(x + 1) && !vis.getOrDefault(x + 1,false)){
-                        q.addLast(x + 1);
-                        vis.put(x + 1,true);
-                        arr[j++] = cnt;
-                    }
-                    if(exist.contains(x - 1) && !vis.getOrDefault(x - 1,false)){
-                        q.addLast(x - 1);
-                        vis.put(x - 1,true);
-                        arr[j++] = cnt;
-                    }
+        int sol = 0; // longest sequence 
+        for(int x : set){
+            if(!set.contains(x - 1)){
+                int cur = x + 1;
+                int len = 1;
+                while(set.contains(cur)){
+                    cur += 1;
+                    len += 1;
                 }
 
-                cnt += 1;
+                sol = Math.max(sol,len);
             }
-                
         }
 
-        int mx = 0;
-        int i = 0 ;j = 0;
-        System.out.println(Arrays.toString(arr));
-        for(;i<arr.length;){
-            if(arr[i] == -1)
-                break;
-            while(j < arr.length && arr[i] == arr[j]){
-                mx = Math.max(mx,j - i + 1);
-                j += 1;
-            }
-            i = j;
-        }
-
-        return mx;
+        return sol;
     }
 }
