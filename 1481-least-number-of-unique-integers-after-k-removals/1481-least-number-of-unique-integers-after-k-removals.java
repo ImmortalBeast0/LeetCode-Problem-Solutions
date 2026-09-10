1a2d683dd57ca5ad4eca4422e73850d2e3ce254c
Monish <168914947+ImmortalBeast0@users.noreply.github.com>
2026-09-10 11:07:36 +0530

    Time: 29 ms (99.47%), Space: 77.9 MB (98.02%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,21 +1,42 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int x : arr)
-            mp.put(x,mp.getOrDefault(x,0)+1);
-        
-        for(int key : mp.keySet())
-            pq.add(new int[]{mp.get(key),key});
+        // in this code logic is 
+        // remove the smallest frequency element first(as much we can) ,to reduce the unique elements in array
 
-        while(k-- > 0){
-            int[] t = pq.poll();
-            t[0] -= 1;
-            if(t[0] > 0)
-                pq.add(t);
+        //so then calculate the frequency of the frequency array 
+        // now we have the freq[freq] ,lets say ,1 freq 5 elements we can delete this element at once 
+        // if we can .... else remove rest of that 
+
+        int freqOfFreq[] = new int[(int)1e5 + 1]; // it much larget ,but it only required less than this
+
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int cnt = 0; // this holds number of uniques in given array
+        for(int x : arr){
+            Integer f = mp.get(x);
+            if(f == null){
+                freqOfFreq[1] += 1;
+                mp.put(x,1);
+                cnt += 1;
+            }else{
+                freqOfFreq[f] -= 1;
+                mp.put(x,f+1);
+                freqOfFreq[f+1] += 1;
+            }
         }
 
-        return pq.size();
+        int i = 1;
+        while(k > 0){
+            if(freqOfFreq[i] * i <= k){
+                k -= freqOfFreq[i] * i;
+                cnt -= freqOfFreq[i];
+            }else{
+                int rem_rq = k / i;
+                cnt -= rem_rq;
+                break;
+            }
+            i += 1;
+        }
+
+        return cnt;
     }
 }
